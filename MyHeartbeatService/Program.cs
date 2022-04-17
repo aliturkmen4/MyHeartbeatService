5c75b8207e4907316f1abf4b9c51399e6a1d50a9
Ali <alym11731996@gmail.com>
2022-04-17 14:12:16 +0300

    Add project files.

---
+++ MyHeartbeatService/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace MyHeartbeatService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+          var exitCode = HostFactory.Run(x=>
+          {
+              x.Service<Heartbeat>(s =>
+              {
+                  s.ConstructUsing(heartbeat => new Heartbeat());
+                  s.WhenStarted(heartbeat => heartbeat.Start());
+                  s.WhenStopped(heartbeat => heartbeat.Stop());
+              });
+
+              x.RunAsLocalSystem();
+
+              x.SetServiceName("HeartbeatService");
+
+              x.SetDisplayName("Heartbeat Service");
+
+              x.SetDescription("Am I alive?");
+
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
